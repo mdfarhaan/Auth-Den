d4727a30fb35076e88a2b27ff2241d7484a2c75f
mdfarhaan <farhaanm110@gmail.com>
2023-06-25 04:16:00 +0530

    :lipstick: Update Button UI

--- client/components/Auth.tsx
+++ client/components/Auth.tsx
@@ -4,11 +4,12 @@ import { API } from '../shared/constants';
 
 const Auth = () => {
   return (
-    <>
+    <div className="grid place-items-center">
       <Button title={'Google'} link={API.auth.google} />
       <Button title={'Github'} link={API.auth.github} />
+      <Button title={'Microsoft'} link={API.auth.microsoft} />
       <Button title={'Twitter'} link={API.auth.twitter} />
-    </>
+    </div>
   );
 };
 
--- client/components/Button.tsx
+++ client/components/Button.tsx
@@ -1,17 +1,59 @@
 import React from 'react';
+import { FiGithub, FiTwitter } from 'react-icons/fi';
+import { FaGoogle } from 'react-icons/fa';
+import { BsMicrosoft } from 'react-icons/bs';
 import { API } from '../shared/constants';
 
 const Button = ({ title, link }) => {
-  return (
-    <div className="m-5">
-      <a
-        href={`${API.API_URL}${API.prefix}${link}`}
-        className="text-xl border p-2 rounded-xl"
-      >
-        Login with {title}
-      </a>
-    </div>
-  );
+  const style =
+    'text-xl border p-2 rounded-xl flex items-center justify-center';
+
+  const renderButton = () => {
+    switch (title) {
+      case 'Google':
+        return (
+          <a
+            href={`${API.API_URL}${API.prefix}${link}`}
+            className={`${style} bg-slate-50`}
+          >
+            Login with {title}
+            <FaGoogle size={22} className="ml-2" />
+          </a>
+        );
+      case 'Github':
+        return (
+          <a
+            href={`${API.API_URL}${API.prefix}${link}`}
+            className={`${style} bg-slate-50`}
+          >
+            Login with {title}
+            <FiGithub size={22} className="ml-2" />
+          </a>
+        );
+      case 'Microsoft':
+        return (
+          <a
+            href={`${API.API_URL}${API.prefix}${link}`}
+            className={`${style} bg-slate-50`}
+          >
+            Login with {title}
+            <BsMicrosoft size={22} className="ml-2" />
+          </a>
+        );
+      case 'Twitter':
+        return (
+          <a
+            href={`${API.API_URL}${API.prefix}${link}`}
+            className={`${style} bg-slate-50`}
+          >
+            Login with {title}
+            <FiTwitter size={22} className="ml-2" />
+          </a>
+        );
+    }
+  };
+
+  return <div className="m-5">{renderButton()}</div>;
 };
 
 export default Button;
